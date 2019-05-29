881caa656b997abd469b5c9567b5cea42e4f14b4
urveeburman <urveeburman59@gmail.com>
2019-05-29 16:24:20 +0530

    server file added

---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.listen(3000, '0.0.0.0', () => {
+    console.log('server started on port 3000');
+})
+  
